bd84254cb02e714d41bf0b822f8674291d60b5cd
fadihub <fadimaalouli@gmail.com>
2015-03-18 22:44:50 -0700

    final working

--- src/main/java/io/advantageous/qbit/example/recommendationengine/UserDataServiceClient.java
+++
@@ -1,8 +0,0 @@
-package io.advantageous.qbit.example.recommendationengine;
-
-import io.advantageous.qbit.service.Callback;
-
-public interface UserDataServiceClient {
-
-    void loadUser(Callback<User> callBack, String userId);
-}
